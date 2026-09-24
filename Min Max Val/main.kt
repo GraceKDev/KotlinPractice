21a0d1c7938a73aac5aab270f8eae1bf7533a307
grace <kearnsgracebusiness@gmail.com>
2026-09-25 00:07:43 +0700

    part 2

---
+++ Min Max Val/main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var number = 23
+    val integerMaxValue = Int.MAX_VALUE 
+    val integerMinValue = Int.MIN_VALUE
+    println("Integer Max: $integerMaxValue")
+    println("Integer Min: $integerMinValue")
+    
+    // cant assign number higher
+    // number  = 2147483648
+    
+    val byteMaxValue = Byte.MAX_VALUE
+    val byteMinValue = Byte.MIN_VALUE
+    println("Byte max value: $byteMaxValue")
+    println("Byte min value: $byteMinValue")
+    
+    val shortMaxValue = Short.MAX_VALUE
+    val shortMinValue = Short.MIN_VALUE
+    
+    println("Short max value: $shortMaxValue")
+    println("Short min value: $shortMinValue")
+    
+}
